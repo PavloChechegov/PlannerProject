0d0e1acbc65558a65fefdc1b999e346194903fd8
PavloChechegov <paulch1991@gmail.com>
2015-09-08 14:10:14 +0300

    Create MainActivity class and layout

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,12 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.pavlo.plannerproject.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
